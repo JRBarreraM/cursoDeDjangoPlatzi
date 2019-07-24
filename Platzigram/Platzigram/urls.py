4de94d5b44484d5ca71fa53a30c21de19e03d9a1
JRBarreraM <mrjrbarreram@gmail.com>
2019-07-24 15:38:30 -0400

    Simple post

--- Platzigram/Platzigram/urls.py
+++ Platzigram/Platzigram/urls.py
@@ -1,9 +1,12 @@
 """Platzigram's URL's module"""
 from django.urls import path
-from Platzigram import views
+from Platzigram import views as local_views
+from posts import views as posts_views
 
 urlpatterns = [
-    path('hello-world/', views.helloWorld),
-    path('sortIntegers/', views.sortIntegers),
-    path('sayHi/<str:name>/<int:age>',views.sayHi),
+    path('hello-world/', local_views.helloWorld),
+    path('sortIntegers/', local_views.sortIntegers),
+    path('sayHi/<str:name>/<int:age>',local_views.sayHi),
+
+    path('posts/',posts_views.list_posts),
 ]
--- Platzigram/posts/views.py
+++ Platzigram/posts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+"""Posts views."""
+# Django
+from django.http import HttpResponse
 
-# Create your views here.
+# Utilities
+from datetime import datetime
+
+posts = [
+    {
+        'name' : 'Mont Blank',
+        'user' : 'Yesica Cortez',
+        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture' : 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name' : 'Mont Blank',
+        'user' : 'Yesica Cortez',
+        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture' : 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name' : 'Mont Blank',
+        'user' : 'Yesica Cortez',
+        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture' : 'https://picsum.photos/200/200/?image=1036',
+    },
+]
+
+def list_posts(request):
+    """List existing posts."""
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
