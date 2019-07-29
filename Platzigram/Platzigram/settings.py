e0ee326a2454454ea285233b54b96fa2f068e8cd
JRBarreraM <mrjrbarreram@gmail.com>
2019-07-29 17:22:35 -0400

    Trying Django forms

--- Platzigram/Platzigram/settings.py
+++ Platzigram/Platzigram/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Platzigram.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'Platzigram.urls'
@@ -60,7 +61,9 @@ ROOT_URLCONF = 'Platzigram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,5 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+
+)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+LOGIN_URL = '/users/login/'
--- Platzigram/Platzigram/urls.py
+++ Platzigram/Platzigram/urls.py
@@ -7,8 +7,13 @@ from django.urls import path
 
 from Platzigram import views as localViews
 from posts import views as postViews
+from users import views as users_views
 
 urlpatterns = [
     path('admin/',admin.site.urls),
-    path('posts/',postViews.list_posts)
+    path('posts/',postViews.list_posts,name='feed'),
+    path('users/login/',users_views.login_view,name='login'),
+    path('users/logout/',users_views.logout_view,name='logout'),
+    path('users/signup/',users_views.signup_view,name='signup'),
+    path('users/me/profile/',users_views.update_profile,name='update_profile'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
